76d09077e6d614b7a9016f62bbd905e55b626d75
jixuni <jixuni@gmail.com>
2020-09-17 09:45:23 -0400

    initial commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div className="ui card">
+      <div className="content">
+        <div className="header">Card Header</div>
+        <div className="meta">Meta</div>
+        <div className="ui divider"></div>
+        <p className="description">
+          Dummy data Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Consequuntur quos aspernatur ducimus at debitis adipisci molestiae
+          quis fugiat voluptate, sequi fuga dicta unde rerum similique incidunt
+          dolor vel porro expedita?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchBar() {
+  return <div>Searchbar component</div>;
+}
+
+export default SearchBar;
