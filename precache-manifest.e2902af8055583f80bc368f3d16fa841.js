823923ef248ed4df77f78c8d978852868c197b93
jixuni <jixuni@gmail.com>
2020-09-24 11:25:09 -0400

    Updates

---
+++ precache-manifest.e2902af8055583f80bc368f3d16fa841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8a1573612c80b7e3022d4cfd6ef50a",
+    "url": "/jixuni/covid-tracker/index.html"
+  },
+  {
+    "revision": "30d37f5989777e05f5f1",
+    "url": "/jixuni/covid-tracker/static/css/main.1234e994.chunk.css"
+  },
+  {
+    "revision": "f9758face783d38eae34",
+    "url": "/jixuni/covid-tracker/static/js/2.94b517c8.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/jixuni/covid-tracker/static/js/2.94b517c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d37f5989777e05f5f1",
+    "url": "/jixuni/covid-tracker/static/js/main.cba2ea1d.chunk.js"
+  },
+  {
+    "revision": "d845857ff5070cea0313",
+    "url": "/jixuni/covid-tracker/static/js/runtime-main.074f543d.js"
+  }
+]);
